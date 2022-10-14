fcd3170ed21260553d0baa6550d62366a43d4b6b
christocarr <christocarr@outlook.com>
2022-10-14 16:59:02 +0100

    feature: add last name input

--- src/features/resume/ResumeBuilder.js
+++ src/features/resume/ResumeBuilder.js
@@ -1,30 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux'
-import { changeName } from './resumeSlice'
+import { useDispatch } from 'react-redux';
+import { changeFormState } from './resumeSlice';
 
 const ResumeBuilder = () => {
-    const navigate = useNavigate();
-    const passedName = useSelector((state) => state.resume.name)
-    const dispatch = useDispatch()
+	const [formState, setFormState] = useState({
+		firstName: '',
+		lastName: '',
+	});
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
 
-    function changeInput(e) {
-      dispatch(changeName(e.target.value));
-    }
+	function changeInput(e) {
+		const name = e.target.name;
+		const value = e.target.value;
+		setFormState({
+			...formState,
+			[name]: value,
+		});
+	}
 
-    const nextPage = () => {
-        navigate('/template');
-    };
-  
-    return (
-      <div>
-        <form>
-          <label htmlFor="name">Your name</label>
-          <input onChange={changeInput} id="name" value={passedName} />
-        </form>
-        <button onClick={nextPage}>next</button>
-      </div>
-    );
-}
+	const nextPage = () => {
+		dispatch(changeFormState(formState));
+		navigate('/template');
+	};
+
+	return (
+		<div>
+			<form>
+				<label htmlFor='firstName'>First Name</label>
+				<input onChange={changeInput} id='firstName' name='firstName' value={formState.firstName} />
+				<label htmlFor='lastName'>Last Name</label>
+				<input onChange={changeInput} id='lastName' name='lastName' value={formState.lastName} />
+			</form>
+			<button onClick={nextPage}>next</button>
+		</div>
+	);
+};
 
 export default ResumeBuilder;
--- src/features/resume/resumeSlice.js
+++ src/features/resume/resumeSlice.js
@@ -1,20 +1,21 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
 
 export const resumeSlice = createSlice({
-  name: 'resume',
-  initialState: {
-    name: "",
-    style: ""
-  },
-  reducers: {
-    changeName: (state, action) => {
-      state.name = action.payload
-    },
-    pickTemplate: (state, action) => {
-      state.style = action.payload   
-    }
-  },
-})
+	name: 'resume',
+	initialState: {
+		formState: {},
+		style: '',
+	},
+	reducers: {
+		changeFormState: (state, action) => {
+			console.log(action, state);
+			state.formState = action.payload;
+		},
+		pickTemplate: (state, action) => {
+			state.style = action.payload;
+		},
+	},
+});
 
-export const { changeName, pickTemplate } = resumeSlice.actions
-export default resumeSlice.reducer
+export const { changeFormState, pickTemplate } = resumeSlice.actions;
+export default resumeSlice.reducer;
