6a30deeb4a878ddaf5774b231e4d44189fc26fbc
christocarr <christocarr@outlook.com>
2022-10-17 17:23:15 +0100

    feat: education input

--- src/features/resume/FinalView.js
+++ src/features/resume/FinalView.js
@@ -3,9 +3,9 @@ import React from 'react';
 import '../../App.css';
 
 const FinalView = () => {
-	const formState = useSelector((state) => state.resume.formState);
+	const resumeData = useSelector((state) => state.resume.resumeData);
 	const pickedTemplate = useSelector((state) => state.resume.style);
-	const { firstName, lastName } = formState;
+	const { firstName, lastName, education } = resumeData;
 	return (
 		<div className={pickedTemplate}>
 			<h5>CV</h5>
@@ -13,6 +13,15 @@ const FinalView = () => {
 				<h2>
 					{firstName} {lastName}
 				</h2>
+				<h3>Education</h3>
+				<ul>
+					{education &&
+						education.map((el) => (
+							<li key={el.university}>
+								<p>{el.university}</p>
+							</li>
+						))}
+				</ul>
 			</div>
 		</div>
 	);
