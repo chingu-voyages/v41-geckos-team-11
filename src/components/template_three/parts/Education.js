fd471231743f52e411c7111b315f32484c2c611b
Roni <ronniiii@yahoo.com>
2022-11-06 23:17:33 +0100

    styling: styled the main component

--- src/components/template_three/parts/Education.js
+++ src/components/template_three/parts/Education.js
@@ -9,26 +9,20 @@ function Education(
 ) {
     return (
         <div className="education">
-            <h2>Education</h2>
-            <div className="row flex">
-                <div className="date">
-                    <p>{startSchoolDate1} 2020/02 - 2022/02 {endSchoolDate1}</p>
-                </div>
-                <div className="details">
-                    {education &&
-                        education.map(el => (
-                            <div>
-                                <h3>{el.degree}: {el.course}</h3>
-                                <p>{el.university} - Nigeria</p>
-                            </div>
-                        ))
-                    }
-                    <div>
-                        <h3>BSc: Computer Science</h3>
-                        <p>University of Toronto - Ontario, Canada</p>
+            <h2>Education</h2> 
+            {education &&
+                education.map(el => (
+                    <div className="flex row">
+                        <div className="date">
+                            <p>{startSchoolDate1} 2020/02 - 2022/02 {endSchoolDate1}</p>
+                        </div>
+                        <div className="details">
+                            <h3>{el.degree}: {el.course}</h3>
+                            <p>{el.university} - Nigeria</p>
+                        </div>
                     </div>
-                </div>
-            </div>
+                ))
+            }
         </div>
     )
 }
--- src/components/template_three/parts/WorkHistory.js
+++ src/components/template_three/parts/WorkHistory.js
@@ -9,16 +9,18 @@ function WorkHistory(
     }
 ) {
     return (
-        <div>
+        <div className="work">
             <h2>Work History</h2>
-            <div className="row">
+            <div className="row flex">
                 <div className="date">
                     <p>{startWorkDate1} - {endWorkDate1}</p>
                 </div>
                 <div className="details">
                     <h3>{jobTitle1}</h3>
                     <p>{company}</p>
-                    <p>{workDescription1}</p>
+                    <p>
+                        {workDescription1}
+                    </p>
                 </div>
             </div>
         </div>
