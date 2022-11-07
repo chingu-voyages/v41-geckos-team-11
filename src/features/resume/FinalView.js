ba2cbe61ff14c38fa3c5c1b72c57f357cb500805
Roni <ronniiii@yahoo.com>
2022-11-07 01:02:35 +0100

    fix: fixed rendering method of final view

--- src/features/resume/FinalView.js
+++ src/features/resume/FinalView.js
@@ -9,15 +9,29 @@ const FinalView = () => {
 	const resumeData = useSelector((state) => state.resume.resumeData);
 	const pickedTemplate = useSelector((state) => state.resume.template);
 
-	return (
-		<div className='wrapper'>
-			{pickedTemplate === `second-template` ? (
-				<TemplateBlue pickedTemplate={pickedTemplate} resumeData={resumeData}  />
-			) : (
+	if (pickedTemplate === `first-template`) {
+		return (
+			<div className="wrapper">
+				<TemplateOne pickedTemplate={pickedTemplate} resumeData={resumeData} />
+			</div>
+		)
+	} else if (pickedTemplate === `second-template`) {
+		return (
+			<div className="wrapper">
+				<TemplateBlue pickedTemplate={pickedTemplate} resumeData={resumeData} />
+			</div>
+		)
+	} else if (pickedTemplate === `third-template`) {
+		return (
+			<div className="wrapper">
 				<TemplateThree pickedTemplate={pickedTemplate} resumeData={resumeData} />
-			)}
-		</div>
-	);
+			</div>
+		)
+	} else {
+		return (
+			<a href="/" className="redirect">Click to Fill Form</a>
+		)
+	}
 };
 
 export default FinalView;
